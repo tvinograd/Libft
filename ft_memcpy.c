a4df1b117d6e25502220906b3b0011fab8a86e48
Tatiana Vinogradova <tatiana.vinogradova@tum.de>
2025-10-16 15:37:49 +0200

    created ft_memcpy.c

---
+++ ft_memcpy.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   tvinogra <tvinogra@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/16 13:13:58 by tvinogra          #+#    #+#             */
+/*   Updated: 2025/10/16 15:36:02 by tvinogra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <stdio.h>
+// #include <string.h>
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	unsigned char	*d;
+	unsigned char	*s;
+
+	d = (unsigned char *)dst;
+	s = (unsigned char *)src;
+	while (n--)
+		*d++ = *s++;
+	return (dst);
+}
+
+// int	main(void)
+// {
+// 	{
+// 	char	src[] = "I love";
+// 	char	dst[] = "I hate programming";
+// 	void *d = ft_memcpy(dst, src, 6);
+// 	printf("dst mine: %s\n", dst);
+// 	}
+//	
+// 	{
+// 	char	src[] = "I love";
+// 	char	dst[] = "I hate programming";
+// 	void *d = memcpy(dst, src, 6);
+// 	printf("dst orig: %s\n", dst);
+// 	}
+//
+// // Tested original function for edge case (NULL) - segfault:
+//
+// 	{
+// 	char	src[] = "I love";
+// 	char	*dst = NULL;
+// 	void *d = memcpy(dst, src, 6);
+// 	printf("dst orig: %s\n", dst);
+// 	}
+//
+// 	return (0);
+// }
