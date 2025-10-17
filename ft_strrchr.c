65c5e52717d23702707d8e019790489812b6250b
Tatiana Vinogradova <tatiana.vinogradova@tum.de>
2025-10-17 10:53:16 +0200

    created ft_strrchr.c

---
+++ ft_strrchr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   tvinogra <tvinogra@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/17 10:42:54 by tvinogra          #+#    #+#             */
+/*   Updated: 2025/10/17 10:52:28 by tvinogra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <stdio.h>
+
+char	*ft_strrchr(const char *s, int c)
+{
+	const char	*last;
+
+	last = NULL;
+	while (*s)
+	{
+		if (*s == (char)c)
+			last = s;
+		s++;
+	}
+	if (c == '\0')
+		return ((char *)s);
+	return ((char *)last);
+}
+
+// int	main(void)
+// {
+// 	printf("%s\n", ft_strrchr("Hello, my name is Tatiana", 'm'));
+// 	return (0);
+// }
