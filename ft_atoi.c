eb5028fb4c5b6f5ef737b0c9f6826b76776e6a8c
Tatiana Vinogradova <tatiana.vinogradova@tum.de>
2025-10-17 14:29:29 +0200

    create ft_atoi.c

---
+++ ft_atoi.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   tvinogra <tvinogra@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/17 13:52:15 by tvinogra          #+#    #+#             */
+/*   Updated: 2025/10/17 14:29:09 by tvinogra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <stdio.h>
+// #include <stdlib.h>
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	sign;
+	int	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+
+	if (!str)
+		return (0);
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
+		|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (sign * result);
+}
+
+// int	main(void)
+// {
+// 	printf("%d\n", ft_atoi("\r\n\t\v\f  -42"));
+// 	printf("%d\n", atoi("\r\n\t\v\f  -42"));
+// 	return (0);
+// }
