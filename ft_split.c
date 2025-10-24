b0a4794f1d69314d22ddef4b1f6a53380b1f5436
Tatiana Vinogradova <tvinogra@3-I-4.42heilbronn.de>
2025-10-24 17:27:09 +0200

    created ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -6,73 +6,125 @@
 /*   By: tvinogra <tvinogra@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/19 21:08:17 by tvinogra          #+#    #+#             */
-/*   Updated: 2025/10/19 23:25:22 by tvinogra         ###   ########.fr       */
+/*   Updated: 2025/10/24 17:21:42 by tvinogra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strcount(char const *s, char c)
+size_t	ft_wordcount(char const *s, char c)
 {
-	int	i;
-	int	str;
+	size_t	i;
+	size_t	n;
 
-	if (!s)
-		return (0);
 	i = 0;
-	str = 0;
+	n = 0;
 	while (s[i])
 	{
 		while (s[i] && s[i] == c)
 			i++;
 		if (!s[i])
 			break ;
-		str++;
+		n++;
 		while (s[i] && s[i] != c)
 			i++;
 	}
-	return (str);
+	return (n);
+}
+
+size_t	ft_wordlen(char const *s, char c)
+{
+	size_t	len;
+
+	len = 0;
+	while (s[len] && s[len] != c)
+		len++;
+	return (len);
+}
+
+char	*ft_strndup(char const *s, size_t len)
+{
+	char	*dst;
+	size_t	i;
+
+	dst = (char *)malloc((len + 1) * sizeof(char));
+	if (!dst)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		dst[i] = s[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
+
+void	ft_freesplit(char **arr, size_t i)
+{
+	while (i > 0)
+	{
+		i--;
+		free(arr[i]);
+	}
+	free(arr);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	char	**array;
+	char	**arr;
 	size_t	i;
-	size_t	start;
-	int		str;
+	size_t	j;
 
 	if (!s)
 		return (NULL);
-	array = (char **)malloc((ft_strcount(s, c) + 1) * sizeof(char *));
-	if (!array)
+	arr = (char **)malloc((ft_wordcount(s, c) + 1) * sizeof(char *));
+	if (!arr)
 		return (NULL);
 	i = 0;
-	str = 0;
+	j = 0;
 	while (s[i])
 	{
 		while (s[i] && s[i] == c)
 			i++;
 		if (!s[i])
 			break ;
-		start = i;
-		while (s[i] && s[i] != c)
-			i++;
-		len = i - start;
-		array[str] = ft_substr(s, (unsigned int)start, len);
-		if (!array[str])
-		{
-			while (str--)
-				free(array[str]);
-			free(array);
-			return (NULL);
-		}
-		str++;
+		arr[j] = ft_strndup(s + i, ft_wordlen(s + i, c));
+		if (!arr[j++])
+			return (ft_freesplit(arr, j - 1), (char **) NULL);
+		i += ft_wordlen(s + i, c);
 	}
-	array[str] = NULL;
-	return (array);
+	arr[j] = NULL;
+	return (arr);
 }
 
-int	main(void)
-{
-	printf("%d\n", ft_strcount("Hello, my name is Tatiana ", ' '));
-}
+// int	main(void)
+// {
+// 	char *input = "Hello, my name is Tatiana ";
+// 	char split = ' ';
+// 	size_t	len;
+// 	char *dst;
+// 	char **arr;
+// 	size_t i;
+
+// printf("Checking ft_wordcount. Amount of words: %zu\n",
+// 	ft_wordcount(input, split));
+// printf("Checking ft_wordlen. Length of first word: %zu\n",
+// 	ft_wordlen(input, split));
+
+// 	len = ft_strlen(input);
+// 	dst = ft_strndup(input, len);
+// 	printf("Checking ft_strndup. String copy: %s\n", dst);
+// 	free(dst);
+
+// 	printf("Checking ft_split. Result: ");
+// 	arr = ft_split(input, split);
+// 	i = 0;
+// 	while (arr[i])
+// 	{
+// 		printf("[%s]", arr[i]);
+// 		i++;
+// 	}
+// 	ft_freesplit(arr, i);
+// 	return (0);
+// }
